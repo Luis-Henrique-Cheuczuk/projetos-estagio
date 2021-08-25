12cc5846b3a90be8881b1702fa10e1c99fa0f0e0
Luis-Henrique-Cheuczuk <luishenriquec0002@gmail.com>
2021-08-25 09:14:49 -0300

    Carousel Manual

---
+++ projeto-web/script.js
@@ -0,0 +1,29 @@
+let indexSlide = 0
+let slides = document.querySelectorAll('div[class="img-fundo"]')
+slides[indexSlide].style.display = "block"
+
+
+function setSlide(index) {
+    indexSlide += index
+
+    // indexSlide = indexSlide % 4
+    // if(indexSlide < 0) {
+    //     indexSlide = slides.length - 1
+    // }
+
+    if (indexSlide < 0) {
+        indexSlide = slides.length - 1
+    }
+
+    if (indexSlide >= slides.length) {
+        indexSlide = 0
+    }
+
+    for (let i = 0; i <= slides.length; i++) {
+        let style = (indexSlide == i) ? "block" : "none"
+        slides[i].style.display = style
+    }
+
+    slides[indexSlide].style.display = "block"
+
+}
