35603f84977e29301fde626f14a76d4aa4312ffa
Luis-Henrique-Cheuczuk <luishenriquec0002@gmail.com>
2021-08-30 09:06:54 -0300

    Alterado as infos dentro do modal, somente os botoes do form nao possuem açao

--- projeto-web/script.js
+++ projeto-web/script.js
@@ -25,7 +25,7 @@ function setSlide(index) {
     }
 }
 //-------------------------------------------------------------
-const btnMenu = document.getElementById('btn-menu')
+const btnMenu = document.getElementsById('btn-menu')
 
 function Menu() {
     const nav = document.getElementById('nav')
@@ -36,14 +36,84 @@ btnMenu.addEventListener('click', Menu)
 
 
 //-------------------------------------------------------------
-const contact = document.getElementById('contact-modal')
+const contact = document.getElementById('contact')
+const signup = document.getElementById('signup')
 
-function ContactModal() {
-    const modalscreen = document.getElementById('modal-screen')
-    modalscreen.addEventListener('click', (e) => {
-        console.log(e.target)
-    })
-    modalscreen.classList.toggle('active')
+function Modal() {
+    const modalcontact = document.getElementById('modal-screen-contact')
+    modalcontact.classList.toggle('active')
 }
 
-contact.addEventListener('click', Contact)
+contact.addEventListener('click', Modal)
+
+//-------------------------------------------------------------
+
+
+//-------------------------------------------------------------
+function limpa_formulário_cep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('rua').value = ("");
+    document.getElementById('bairro').value = ("");
+    document.getElementById('cidade').value = ("");
+    document.getElementById('uf').value = ("");
+    document.getElementById('ibge').value = ("");
+}
+
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        document.getElementById('rua').value = (conteudo.logradouro);
+        document.getElementById('bairro').value = (conteudo.bairro);
+        document.getElementById('cidade').value = (conteudo.localidade);
+        document.getElementById('uf').value = (conteudo.uf);
+        document.getElementById('ibge').value = (conteudo.ibge);
+    } //end if.
+    else {
+        //CEP não Encontrado.
+        limpa_formulário_cep();
+        alert("CEP não encontrado.");
+    }
+}
+
+function pesquisacep(valor) {
+
+    //Nova variável "cep" somente com dígitos.
+    var cep = valor.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
+
+        //Valida o formato do CEP.
+        if (validacep.test(cep)) {
+
+            //Preenche os campos com "..." enquanto consulta webservice.
+            document.getElementById('rua').value = "...";
+            document.getElementById('bairro').value = "...";
+            document.getElementById('cidade').value = "...";
+            document.getElementById('uf').value = "...";
+            document.getElementById('ibge').value = "...";
+
+            //Cria um elemento javascript.
+            var script = document.createElement('script');
+
+            //Sincroniza com o callback.
+            script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback';
+
+            //Insere script no documento e carrega o conteúdo.
+            document.body.appendChild(script);
+
+        } //end if.
+        else {
+            //cep é inválido.
+            limpa_formulário_cep();
+            alert("Formato de CEP inválido.");
+        }
+    } //end if.
+    else {
+        //cep sem valor, limpa formulário.
+        limpa_formulário_cep();
+    }
+}
